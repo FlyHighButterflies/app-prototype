b55494c5d7c34032b85d0baa2f05614f70c66632
SntJhn <jagdevera@gmail.com>
2025-01-08 17:46:32 +0800

    Add Back-end

---
+++ payamonte/src/main/java/com/flyhighbutterflies/payamonte/PayamonteApplication.java
@@ -0,0 +1,13 @@
+package com.flyhighbutterflies.payamonte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayamonteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayamonteApplication.class, args);
+	}
+
+}
---
+++ payamonte/src/test/java/com/flyhighbutterflies/payamonte/PayamonteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flyhighbutterflies.payamonte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayamonteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
