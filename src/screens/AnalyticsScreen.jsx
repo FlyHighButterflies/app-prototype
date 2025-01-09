f9b8c0db160a8740699f03478c7b8db996ae9ca1
Alingggg <lapitangeof2@gmail.com>
2025-01-09 17:26:39 +0800

    update AnalyticsScreen
    change line graph to bar graph

--- src/screens/AnalyticsScreen.jsx
+++ src/screens/AnalyticsScreen.jsx
@@ -1,16 +1,62 @@
-import ExpenseItem from "components/ExpenseItem";
-import { useData } from "context/DataContext";
-import { useEffect, useState } from "react";
+import { LinearGradient, useFont, vec } from "@shopify/react-native-skia"
 import { Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { CartesianChart, Line } from "victory-native";
+import { Bar, CartesianChart } from "victory-native";
+import roboto from "./Roboto-Regular.ttf";
 
-const DATA = Array.from({ length: 31 }, (_, i) => ({
-  day: i,
-  highTmp: 40 + 30 * Math.random(),
-}));
+const DATA = [
+  {
+    month: 1,
+    expense: 1,
+  },
+  {
+    month: 2,
+    expense: 2,
+  },
+  {
+    month: 3,
+    expense: 3,
+  },
+  {
+    month: 4,
+    expense: 2,
+  },
+  {
+    month: 5,
+    expense: 2,
+  },
+  {
+    month: 6,
+    expense: 2,
+  },
+  {
+    month: 7,
+    expense: 2,
+  },
+  {
+    month: 8,
+    expense: 2,
+  },
+  {
+    month: 9,
+    expense: 2,
+  },
+  {
+    month: 10,
+    expense: 2,
+  },
+  {
+    month: 11,
+    expense: 2,
+  },
+  {
+    month: 12,
+    expense: 2,
+  },
+];
 
 function AnalyticsScreen({ navigation }) {
+  const myFont = useFont(roboto, 12)
 
   return (
     <SafeAreaView style={style.screenContainer}>
@@ -27,11 +73,53 @@ function AnalyticsScreen({ navigation }) {
           <Text style={style.headerText}>Analytics</Text>
         </View>
       </View>
-      <View style={{ justifyContent: "center", alignItems: "center" }}>
+
+      {/* Graph */}
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 15,
+        }}
+      >
         <View style={style.chartContainer}>
-          <CartesianChart data={DATA} xKey="day" yKeys={["highTmp"]}>
-            {({ points }) => (
-              <Line points={points.highTmp} color="red" strokeWidth={3} />
+          <CartesianChart
+            data={DATA}
+            xKey="month"
+            yKeys={["expense"]}
+            domain={{ x: [1, 12], y: [0, 10] }}
+            domainPadding={{ left: 15, right: 15 }}
+            padding={10}
+            xAxis={{
+              font: myFont,
+              formatXLabel: (value) => {
+                const date = new Date();
+                date.setMonth(value - 1);
+                const monthName = date.toLocaleString("default", {
+                  month: "short",
+                });
+                return monthName;
+              },
+            }}
+          >
+            {({ points, chartBounds }) => (
+              <Bar
+                points={points.expense}
+                chartBounds={chartBounds}
+                barWidth={10}
+                roundedCorners={{
+                  topLeft: 20,
+                  topRight: 20,
+                  bottomLeft: 20,
+                  bottomRight: 20,
+                }}
+              >
+                <LinearGradient
+                  start={vec(0, 0)}
+                  end={vec(0, 400)}
+                  colors={["#a78bfa", "#a78bfa50"]}
+                />
+              </Bar>
             )}
           </CartesianChart>
         </View>
@@ -62,7 +150,6 @@ const style = {
     height: 300,
     width: "100%",
     borderWidth: 1,
-    marginTop: 15,
   },
 };
 
