e3b419fff5892d13a61b85511bd1df7da0ddfdba
Alingggg <lapitangeof2@gmail.com>
2025-01-20 02:00:53 +0800

    Update AnalyticsScree
    - remove back button
    - add expense per category list

--- src/screens/AnalyticsScreen.jsx
+++ src/screens/AnalyticsScreen.jsx
@@ -1,8 +1,10 @@
-import { LinearGradient, useFont, vec } from "@shopify/react-native-skia"
-import { Text, TouchableOpacity, View } from "react-native";
+import { LinearGradient, useFont, vec } from "@shopify/react-native-skia";
+import { ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Bar, CartesianChart } from "victory-native";
 import roboto from "./Roboto-Regular.ttf";
+import { useState } from "react";
+import { useData } from "context/DataContext";
 
 const DATA = [
   {
@@ -55,20 +57,41 @@ const DATA = [
   },
 ];
 
-function AnalyticsScreen({ navigation }) {
-  const myFont = useFont(roboto, 12)
+function CategoryItem({ style: outerStyle, category, amount }) {
+  return (
+    <View style={{ ...outerStyle, ...style.itemContainer }}>
+      <View style={style.infoContainer}>
+        <View>
+          <Text>{category}</Text>
+        </View>
+        <View style={style.amountContainer}>
+          <Text style={style.amount}>${amount}</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+function AnalyticsScreen() {
+  const myFont = useFont(roboto, 12);
+  const [transactions, setTransactions] = useState(useData());
+  const dataByCategory = transactions.reduce((finalObject, transaction) => {
+    const { category, amount } = transaction;
+
+    if (!finalObject[category]) {
+      finalObject[category] = { total: 0, transactions: [] };
+    }
+
+    finalObject[category].total += amount;
+    finalObject[category].transactions.push(transaction);
+    return finalObject;
+  }, {});
+
+  console.log(dataByCategory);
 
   return (
     <SafeAreaView style={style.screenContainer}>
       <View style={style.headerContainer}>
-        <TouchableOpacity
-          style={style.headerBackContainer}
-          onPressOut={() => {
-            navigation.goBack();
-          }}
-        >
-          <Text>Back</Text>
-        </TouchableOpacity>
         <View>
           <Text style={style.headerText}>Analytics</Text>
         </View>
@@ -124,6 +147,22 @@ function AnalyticsScreen({ navigation }) {
           </CartesianChart>
         </View>
       </View>
+      
+      <ScrollView
+        style={style.categoryItemListContainer}
+        showsVerticalScrollIndicator={false}
+      >
+        {Object.entries(dataByCategory).map(([key, value]) => {
+          return (
+            <CategoryItem
+              key={key}
+              style={style.categoryItemContainer}
+              category={key}
+              amount={value.total}
+            />
+          );
+        })}
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -135,6 +174,7 @@ const style = {
   },
   headerContainer: {
     flexDirection: "row",
+    justifyContent: "center",
   },
   headerBackContainer: {
     borderWidth: 1,
@@ -150,6 +190,34 @@ const style = {
     height: 300,
     width: "100%",
     borderWidth: 1,
+  },
+  itemContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "50",
+    padding: 10,
+    paddingHorizontal: "20",
+    borderWidth: 1,
+    gap: 20,
+  },
+  infoContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  amountContainer: {
+    justifyContent: "center",
+  },
+  categoryItemListContainer: {
+    marginTop: 20,
+    // borderWidth: 1,
+    borderColor: "red",
+  },
+  categoryItemContainer: {
+    marginBottom: 10,
+    borderRadius: 10,
+    // borderWidth: 1,
   },
 };
 
