594036c6da6c711735a9598885239520bd4e4495
Alingggg <lapitangeof2@gmail.com>
2025-01-29 13:15:53 +0800

    add BottomNavigation icons

--- src/navigation/BottomNavigation.jsx
+++ src/navigation/BottomNavigation.jsx
@@ -4,33 +4,42 @@ import AnalyticsScreen from "screens/AnalyticsScreen";
 import HomeScreen from "screens/HomeScreen";
 import NewAnalyticsScreen from "screens/NewAnalyticsScreen";
 import TestScreen from "screens/TestScreen";
+import HomeIcon from "react-native-vector-icons/Feather";
+import AnalyticsIcon from "react-native-vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
+const ICON_COLOR = "#800000";
+const ICON_SIZE = 25;
 
 function BottomNavigation() {
-
   return (
-    <Tab.Navigator initialRouteName="Analytics">
-      <Tab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{ headerShown: false }}
-      />
-      <Tab.Screen
-        name="FakeAnalytics"
-        component={AnalyticsScreen}
-        options={{ headerShown: false }}
-      />
-      <Tab.Screen
-        name="Analytics"
-        component={NewAnalyticsScreen}
-        options={{ headerShown: false }}
-      />
-      <Tab.Screen
-        name="Test"
-        component={TestScreen}
-        options={{ headerShown: false }}
-      />
+    <Tab.Navigator
+      initialRouteName="Analytics"
+      screenOptions={({ route }) => ({
+        tabBarIcon: () => {
+          if (route.name === "Home") {
+            return <HomeIcon name="home" size={ICON_SIZE} color={ICON_COLOR} />;
+          } else if (route.name === "Analytics") {
+            return (
+              <AnalyticsIcon
+                name="analytics-outline"
+                size={ICON_SIZE}
+                color={ICON_COLOR}
+              />
+            );
+          }
+          return null;
+        },
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: ICON_COLOR,
+        tabBarInactiveTintColor: ICON_COLOR,
+        headerShown: false,
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="FakeAnalytics" component={AnalyticsScreen} />
+      <Tab.Screen name="Analytics" component={NewAnalyticsScreen} />
+      <Tab.Screen name="Test" component={TestScreen} />
     </Tab.Navigator>
   );
 }
